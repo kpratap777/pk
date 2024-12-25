6797da0df9063b4226f0bc816d637b1868172a05
kpratap777 <khadkapratap00@gmail.com>
2024-12-25 17:36:00 +0000

    Add files via upload

---
+++ week 1/example.2.js
@@ -0,0 +1,12 @@
+const x = 10;
+const y = 2;
+// two numbers
+const addition = x + y;
+const subtraction = x - y;
+const multiplication = x * y;
+const division = x / y;
+// display 
+console.log('The addition of ' + x + ' and ' + y + ' is: ' + addition);
+console.log('The subtraction of ' + x + ' and ' + y + ' is: ' + subtraction);
+console.log('The multiplication of ' + x + ' and ' + y + ' is: ' + multiplication);
+console.log('The division of ' + x + ' and ' + y + ' is: ' + division);
---
+++ week 3/index.js
@@ -0,0 +1,16 @@
+// defination of the function EmployeeInfo
+function EmployeeInfo(name,Salary)
+{
+console.log("Wellcome " + name+ "Your monthly Salary is "+ Salary)
+}
+console.log ("This is my first progame")
+var EmpName="John"
+var EmpSalary= 50000
+// calling of the function EmployeeInfo
+EmployeeInfo(EmpName,EmpSalary)
+EmployeeInfo(EmpName,EmpSalary)
+//Code for Second Exercise starts from here:
+const EmpSkills= (skills)=> {
+ console.log("Expert in "+ skills)
+}
+EmpSkills("java")
